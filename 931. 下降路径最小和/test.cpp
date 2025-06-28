22088126b1297db0f6a2f10bdd9f06246e255afd
Jidong-Zhang <94980508+ZJidong2001@users.noreply.github.com>
2025-06-28 16:09:49 +0800

    下降路径最小和

---
+++ 931. 下降路径最小和/test.cpp
@@ -0,0 +1,17 @@
+class Solution
+{
+public:
+    int minFallingPathSum(vector<vector<int>>& matrix)
+    {
+        vector<vector<int>> dp(matrix.size() + 1, vector<int>(matrix.size() + 2, INT_MAX));
+        for (int& x : dp[0]) x = 0;
+
+        for (int i = 1; i <= matrix.size(); ++i)
+            for (int j = 1; j < matrix.size(); ++j)
+                dp[i][j] = min(dp[i - 1][j], min(dp[i - 1][j - 1], dp[i - 1][j + 1])) + matrix[i - 1][j - 1];
+
+        int ret = INT_MAX;
+        for (int e : dp.back()) ret = min(e, ret);
+        return ret;
+    }
+};
