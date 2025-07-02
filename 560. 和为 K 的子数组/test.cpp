2b08e08d6acc3ca89d671607abf522d2e1cd4ff6
Jidong-Zhang <94980508+ZJidong2001@users.noreply.github.com>
2025-07-02 21:54:19 +0800

    和为 K 的子数组

---
+++ 560. 和为 K 的子数组/test.cpp
@@ -0,0 +1,17 @@
+class Solution
+{
+public:
+    int subarraySum(vector<int>& nums, int k)
+    {
+        int ret = 0;
+        unordered_map<int, int> hash;
+        hash[0] = 1;
+        for (int i = 0, sum = 0; i < nums.size(); ++i)
+        {
+            sum += nums[i];
+            if (hash.count(sum - k)) ret += hash[sum - k];
+            ++hash[sum];
+        }
+        return ret;
+    }
+};
